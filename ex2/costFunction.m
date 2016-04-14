372a7180172f1a016db6afa34c38a69be56329a8
20132006 <taalaibekuulualibek@gmail.com>
2016-04-14 11:45:15 +0900

    CostFunction
    completed

--- ex2/costFunction.m
+++ ex2/costFunction.m
@@ -20,9 +20,11 @@ grad = zeros(size(theta));
 % Note: grad should have the same dimensions as theta
 %
 
-grad = sigmoid(theta'*X');
+h = sigmoid(theta'*X');
 
-J = (1/m)*sum( (-y*log(grad)) - ( (1-y)*log(1-grad) ) );
+J = (1/m)*sum( (-y*log(h)) - ( (1-y)*log(1-h) ) );
+
+grad = (1/m) * sum(h-y)*X;
 
 % =============================================================
 
